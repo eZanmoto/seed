75960b917242bffdbc6b4c5c09d2b7b73236981c
Sean Kelleher <ezanmoto@gmail.com>
2023-12-17 00:25:55 +0000

    Simplify repeated error construction

--- src/eval/bind.rs
+++ src/eval/bind.rs
@@ -33,7 +33,9 @@ fn bind_next(
     -> Result<(), Error>
 {
     let (raw_lhs, (line, col)) = lhs;
-    let new_loc_error = |source| {
+    let new_invalid_bind_error = |s: &str| {
+        let source = Error::InvalidBindTarget{descr: s.to_string()};
+
         Err(Error::AtLoc{source: Box::new(source), line, col})
     };
 
@@ -41,36 +43,12 @@ fn bind_next(
         RawExpr::Var{name} => {
             bind_next_name(scopes, names_in_binding, name, (line, col), rhs)
         },
-        RawExpr::Null => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "`null`".to_string(),
-            })
-        },
-        RawExpr::Bool{..} => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "a boolean literal".to_string(),
-            })
-        },
-        RawExpr::Int{..} => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "an integer literal".to_string(),
-            })
-        },
-        RawExpr::Str{..} => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "a string literal".to_string(),
-            })
-        },
-        RawExpr::List{..} => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "a list literal".to_string(),
-            })
-        },
-        RawExpr::Call{..} => {
-            new_loc_error(Error::InvalidBindTarget{
-                descr: "a function call".to_string(),
-            })
-        },
+        RawExpr::Null => new_invalid_bind_error("`null`"),
+        RawExpr::Bool{..} => new_invalid_bind_error("a boolean literal"),
+        RawExpr::Int{..} => new_invalid_bind_error("an integer literal"),
+        RawExpr::Str{..} => new_invalid_bind_error("a string literal"),
+        RawExpr::List{..} => new_invalid_bind_error("a list literal"),
+        RawExpr::Call{..} => new_invalid_bind_error("a function call"),
     }
 }
 
